20c287ed68d644d66690d1e5b47f16daaf02d39d
Kiara Guzman <kiara.kg11@gmail.com>
2019-07-03 14:00:48 -0400

    vars created in javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    
+    var wins = 0;
+    var losses = 0;
+    var userTotal = 0;
+  
+    
+    var randomNumber = Math.floor(Math.random() * 102 + 19);
+    var amethystNumber = Math.floor(Math.random() * 12 + 1);
+    var quartzNumber = Math.floor(Math.random() * 12 + 1);
+    var emeraldNumber = Math.floor(Math.random() * 12 + 1);
+    var rubyNumber = Math.floor(Math.random() * 12 + 1);
